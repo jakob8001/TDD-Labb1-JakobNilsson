6e6be11bd35c0afc2382635fd8467f481d8c44dc
Jakob Nilsson <jakob.nilsson@iths.se>
2022-02-15 09:44:47 +0100

    Green!

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -1,7 +1,15 @@
 public class Employee {
 
+    private int ID;
+    private static int counter;
+
 public Employee(String Fname, String Ename, int age, double Salary){
 
+        counter = counter + 1;
+        ID = counter;
     }
 
+    public int getUniqueID() {
+        return ID;
+    }
 }
